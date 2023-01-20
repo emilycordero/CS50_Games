dcaa83d8f38550dbb74fb5bcfcf14ff7b6ba4556
emilycordero <cordeemi031@gmail.com>
2023-01-19 20:42:17 -0800

    The Paddle update

--- Pong/Pong2/main.lua
+++ Pong/Pong2/main.lua
@@ -17,30 +17,71 @@ push = require 'push'
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
--- We are adding the dimensions for the virtual window
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+-- We are declaring the speed the paddles will move at. In the update it will be multiplied by dt.
+PADDLE_SPEED = 200
+
 -- Initializes the game 
 function love.load()
-    -- We are going to add the 'nearest neighbor' filtering to help prevent blrring of text/graphics
+    
     love.graphics.setDefaultFilter('nearest', 'nearest')
+    -- We are going to "seed" the RNG that way the calls to random are always random. We will use the current time to make sure it will vary every time at start
+    math.randomseed(os.time())
 
     -- We are adding the retro font for any text. We reference the font file and add the size
     smallFont = love.graphics.newFont('font.ttf', 8)
+    scoreFont = love.graphics.newFont('font.ttf', 32)
 
-
-
-
+    -- Set the active LOVE2D font to the smallFont
+    love.graphics.setFont(smallFont)
 
 push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
     fullscreen = false, 
     resizable = false,
     vsync = true
 })
+
+-- Initialize score variables
+player1Score = 0
+player2Score = 0
+
+-- We are putting the paddle positions on the y axis that way they can only move up/down
+player1Y = 30
+player2Y = VIRTUAL_HEIGHT - 50
+
+-- We are declaring the velocity and position variables for when the ball is in the play state
+ballX = VIRTUAL_WIDTH / 2 -2
+ballY = VIRTUAL_HEIGHT / 2 -2
+
+-- math.random returns a random value between the 2 numbers that we want
+ballDX = math.random(2) == 1 and 100 or -100
+ballDY = math.random(-50,50)
+
+-- The game state variable is used to help transition between different parts of the game (beginning, menus, main game, etc.)The goal is to use this to help determine behavior during rendering and updating. 
+gameState = 'start'
 end
 
--- We are adding keyboard handling which is called by LOVE2D at each frame and it passes in the key we press to have access.
+-- Runs every frame with 'dt' passed in 
+-- for the player 1 movement We want to add negative paddle speed to current Y scaled by deltaTime, we are setting bounds on the screen and math.max returns the greater of the 2 values and 0 and player Y makes sure we don't go over. Then when s is pressed we want to add positive paddle speed. 
+function love.update(dt)
+    -- Player 1 movement
+    if love.keyboard.isDown('w') then 
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    -- Player 2 movement 
+    if love.keyboard.isDown('up') then 
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+end
+
+-- Keyboard handling which is called by LOVE2D at each frame and it passes in the key we press to have access.
 function love.keypressed(key)
     -- Keys can be accessed by string name and we will use a function LOVE2D gives us to terminate app
     if key == 'escape' then
@@ -56,13 +97,20 @@ function love.draw()
 
         love.graphics.clear(40/255, 25/255, 52/255, 255/255)
 
+        -- Swapping the font before printing
+        love.graphics.setFont(smallFont)
         love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
-        -- Rendering the first paddle to be on the left side of the screen
-        love.graphics.rectangle('fill', 10,30,5,20)
+        -- Draw score on the right and left center of the screen
+        love.graphics.setFont(scoreFont)
+        love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+        love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
+        -- Rendering the first paddle to be on the left side of the screen by using the players Y variable
+        love.graphics.rectangle('fill', 10, player1Y,5,20)
 
         -- Rendering the second paddle to be on the right side of the screen 
-        love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT -50, 5, 20)
+        love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
         -- Rendering the ball at the cnenter of the screen 
         love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 -2, 4, 4)
